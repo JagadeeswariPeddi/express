3042e5560d5b322eaeace208050db2ec6b493995
JagadeeswariPeddi <chinnipeddi3126@gmail.com>
2022-11-23 12:08:01 +0530

    express

---
+++ data.js
@@ -0,0 +1,8 @@
+const students=[
+{stdid:1,stdname:"ram",stdclass:"mpc"},
+{stdid:2,stdname:"janani",stdclass:"cec"},
+{stdid:3,stdname:"nani",stdclass:"mec"},
+{stdid:4,stdname:"chinni",stdclass:"bipc"},
+{stdid:5,stdname:"raj",stdclass:"hec"},
+]
+module.exports=students
